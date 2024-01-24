04ff448d3672e299fda60a1a35e5bb8854291d56
Daylof <90395763+Daylof05@users.noreply.github.com>
2024-01-24 21:42:21 +0100

    implementation code samir

--- src/folders/main.go
+++ src/folders/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -16,6 +18,16 @@ func main() {
 		renameFolder(os.Args[2], os.Args[3])
 	case "deletefolder":
 		deleteFolder(os.Args[2])
+	case "createfile":
+		creationfile(os.Args[2], os.Args[3])
+	case "readfile":
+		readfile(os.Args[2])
+	case "renamefile":
+		renamefile(os.Args[2], os.Args[3])
+	case "changecontenufile":
+		Changecontenufile(os.Args[2], os.Args[3])
+	case "deletefile":
+		DeleteFile(os.Args[2])
 	}
 }
 
@@ -33,4 +45,34 @@ func renameFolder(oldTitle string, newTitle string) {
 
 func deleteFolder(title string) {
 	os.RemoveAll(title)
+}
+
+func creationfile(title string, content string) {
+	// Création
+	fichier, _ := os.Create(title)
+	io.WriteString(fichier, content)
+	fichier.Close()
+}
+
+func readfile(title string) {
+	// Lecture
+	read, _ := ioutil.ReadFile(title)
+	fmt.Println(string(read))
+}
+
+func renamefile(title string, newtitle string) {
+	// Renommer
+
+	os.Rename(title, newtitle)
+}
+
+func Changecontenufile(title string, content string) {
+	// Changer contenu
+	fichier, _ := os.OpenFile(title, os.O_WRONLY|os.O_TRUNC, 0666)
+	io.WriteString(fichier, content)
+	fichier.Close()
+}
+
+func DeleteFile(title string) {
+	os.Remove(title)
 }
